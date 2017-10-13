c0e09443888fc49a09b7671cb79c9e3ee1b4bf2d
Daniel Gading <daniel.gading@agencychief.com>
2017-10-13 12:38:59 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    author: `Daniel Gading`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: `pages`,
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: []
+      }
+    }
+  ],
+}
---
+++ src/pages/10-05-2017-getting-started/index.md
@@ -0,0 +1,7 @@
+---
+path: "/hello-world"
+date: "2017-10-05T12:19:33.962Z"
+title: "My First Post"
+---
+
+Ooooogachaaka a blog post!
